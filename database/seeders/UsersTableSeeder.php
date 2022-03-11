b91738bcee59293d346930243dc3ba55778b4e5e
Yasser Guillermo <elguilleferrer@gmail.com>
2022-03-11 05:50:24 -0500

    Semillas de usuario definitivas

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -16,9 +16,21 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         User::create([
-            'name' => 'Administrador',
-            'email' => 'admin@comercioij.co.cu',
-            'password' => bcrypt('admin'),
+            'name' => 'Guille',
+            'email' => 'yalvarez@comercioij.co.cu',
+            'password' => bcrypt('Password.01asd$'),
+        ]);
+
+        User::create([
+            'name' => 'Yuliet Lusson',
+            'email' => 'ylusson@comercioij.co.cu',
+            'password' => bcrypt('Ylusson2022*-'),
+        ]);
+
+        User::create([
+            'name' => 'Yarisleydis Rodríguez',
+            'email' => 'yrodriguez@comercioij.co.cu',
+            'password' => bcrypt('Yarita2021*'),
         ]);
     }
 }
