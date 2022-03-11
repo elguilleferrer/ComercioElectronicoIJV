160804d7d48899165eb1d6b3bb40c3d5a6f27f43
Yasser Guillermo <elguilleferrer@gmail.com>
2022-03-11 05:47:42 -0500

    Evitando la divicion por cero

--- resources/views/estadisticas/index.blade.php
+++ resources/views/estadisticas/index.blade.php
@@ -15,9 +15,11 @@
                     <div class="col-sm-6">
                         <h4 class="text-center">Ventas total Acumulado Comercio Electrónico: <strong class="text-success">${{number_format($acumuladoComercioElectronico,2)}}</strong></h4>
                     </div>
-                    <div class="col-12 text-center">
-                        <h5>Esto representa un <strong class="text-success">%{{number_format(($acumuladoComercioElectronico*100)/$acumuladoEfectivoCredito,2)}}</strong></h5>
-                    </div>
+                    @if($acumuladoEfectivoCredito > 0)
+                        <div class="col-12 text-center">
+                            <h5>Esto representa un <strong class="text-success">%{{number_format(($acumuladoComercioElectronico*100)/$acumuladoEfectivoCredito,2)}}</strong></h5>
+                        </div>
+                    @endif
                     <div class="col-12">
                         <h3>Acumulados por tipo de unidad</h3>
                         <div class="row">
@@ -32,9 +34,9 @@
                                                 ${{number_format($tipoUnidad->acumuladoCE()->sum(DB::raw("transfer_movil + post + enzona + tienda_virtual")),2)}}
                                             </h1>
                                         </div>
-{{--                                        <div class="card-footer">--}}
-{{--                                            <small class="text-black-50">Última actualización: <strong>{{$tipoUnidad->ultimaActualizacion()}}</strong></small>--}}
-{{--                                        </div>--}}
+                                        {{--                                        <div class="card-footer">--}}
+                                        {{--                                            <small class="text-black-50">Última actualización: <strong>{{$tipoUnidad->ultimaActualizacion()}}</strong></small>--}}
+                                        {{--                                        </div>--}}
                                     </div>
                                 </div>
                             @endforeach
