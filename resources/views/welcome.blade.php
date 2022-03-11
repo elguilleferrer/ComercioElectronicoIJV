2ffae5989fc5e7a5ab749923ec3f8abec5b880d6
Yasser Guillermo <elguilleferrer@gmail.com>
2022-03-11 05:55:12 -0500

    Titulo de la pagina de inicio

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>ComercioElectronicoIJV</title>
 
     <!-- Fonts -->
     <link href="{{asset('css/nunito.css')}}?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
